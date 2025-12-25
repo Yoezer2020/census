e7c72a9a3e279e36d40429676585b9b65abfda87
Asseh Nepal <upadhaya.asseh@gmail.com>
2025-12-25 10:38:27 +0600

    build issues fixes

--- nestjs-mysql-template/src/common/dto/create-translation.dto.ts
+++ nestjs-mysql-template/src/common/dto/create-translation.dto.ts
@@ -1,6 +1,10 @@
 import { StringField } from '../../decorators/field.decorators.ts';
+import { LanguageCode } from '../../constants/language-code.ts';
 
 export class CreateTranslationDto {
+  @StringField()
+  languageCode!: LanguageCode;
+
   @StringField()
   text!: string;
 }
